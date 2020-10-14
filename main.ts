9328a0d31c15da1a2e53b32a766ad323f96ec41c
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-10-14 15:26:15 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@ basic.forever(function () {
     basic.clearScreen()
     basic.pause(100)
     for (let index = 0; index <= 9; index++) {
+        if (index == 5) {
+            continue;
+        }
         basic.showNumber(index)
         basic.pause(100)
         basic.clearScreen()
         basic.pause(100)
-        if (index == 5) {
-            break;
-        }
     }
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
